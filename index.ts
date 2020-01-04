60caa986f862658cc3da1cedc996ea0cd1854ed7
Philip Waritschlager <philip@waritschlager.de>
2020-01-04 15:18:00 +0100

    make repository work with up to date deno version

--- index.ts
+++ index.ts
@@ -1,9 +1,7 @@
-import * as deno from "deno";
-
 const readFile = async (file: string, type?: string) => {
   const types = type || 'utf-8'
   return new TextDecoder(types).decode(
-    await deno.readFile(file)
+    await Deno.readFile(file)
   )
 }
 
